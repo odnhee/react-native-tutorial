510db552dc73277f9ab0d952f006963fe91262a7
HyungJinHan <han1210_36@naver.com>
2023-06-04 17:17:50 +0900

    장바구니 구현 진행 중

--- delivery-app/client/components/BasketIcon.jsx
+++ delivery-app/client/components/BasketIcon.jsx
@@ -1,10 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectBasketItems, selectBasketTotal } from "../features/basketSlice";
+import { useNavigation } from "@react-navigation/native";
+import Currency from "react-currency-formatter";
 
 const BasketIcon = () => {
+  const items = useSelector(selectBasketItems);
+  const navigation = useNavigation();
+  const basketTotal = useSelector(selectBasketTotal);
+
   return (
-    <View>
-      <Text>BasketIcon</Text>
+    <View className="absolute bottom-10 w-full z-50">
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Basket")}
+        className="bg-[#00CCBB] mx-5 p-4 rounded-xl flex-row items-center space-x-1"
+      >
+        <View className="bg-[#01A296] py-1 px-2 rounded-full w-12 h-12 items-center justify-center">
+          <Text className="text-white font-extrabold text-lg text-center">
+            {items.length}
+          </Text>
+        </View>
+        <Text className="flex-1 text-white font-extrabold text-lg text-center">
+          View Basket
+        </Text>
+
+        <Text className="text-lg text-white font-extrabold">
+          <Currency quantity={basketTotal} currency="KRW" pattern="###,### !" />
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- delivery-app/client/screens/RestaurantScreen.jsx
+++ delivery-app/client/screens/RestaurantScreen.jsx
@@ -6,7 +6,7 @@ import {
   TouchableOpacity,
   Platform,
 } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { urlFor } from "../sanity";
 import {
@@ -18,9 +18,15 @@ import {
 import { StarIcon } from "react-native-heroicons/solid";
 import { globalStyles } from "../config/globalStyles";
 import DishRow from "../components/DishRow";
+import BasketIcon from "../components/BasketIcon";
+import { selectBasketItems } from "../features/basketSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { setRestaurant } from "../features/restaurantSlice";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
+  const items = useSelector(selectBasketItems);
+  const dispatch = useDispatch();
 
   const {
     params: {
@@ -37,6 +43,23 @@ const RestaurantScreen = () => {
     },
   } = useRoute();
 
+  useEffect(() => {
+    dispatch(
+      setRestaurant({
+        id,
+        imgUrl,
+        title,
+        rating,
+        genre,
+        address,
+        shortDescription,
+        dishes,
+        long,
+        lat,
+      })
+    );
+  }, []);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -44,79 +67,85 @@ const RestaurantScreen = () => {
   }, []);
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <View className="bg-white">
-        <View>
-          <Image
-            source={{ uri: urlFor(imgUrl).url() }}
-            className="w-full h-56 bg-gray-300 p-4"
-          />
+    <>
+      {items.length > 0 ? <BasketIcon /> : null}
 
-          {Platform.OS === "ios" ? (
-            <TouchableOpacity
-              onPress={navigation.goBack}
-              className="absolute top-14 left-5 bg-white rounded-full w-8 h-8 items-center justify-center"
-            >
-              <ArrowLeftIcon size={20} color={"#00CCBB"} />
-            </TouchableOpacity>
-          ) : null}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className="bg-white relative">
+          <View>
+            <Image
+              source={{ uri: urlFor(imgUrl).url() }}
+              className="w-full h-56 bg-gray-300 p-4"
+            />
+
+            {Platform.OS === "ios" ? (
+              <TouchableOpacity
+                onPress={navigation.goBack}
+                className="absolute top-14 left-5 bg-white rounded-full w-8 h-8 items-center justify-center"
+              >
+                <ArrowLeftIcon size={20} color={"#00CCBB"} />
+              </TouchableOpacity>
+            ) : null}
+          </View>
+
+          <View style={globalStyles.container}>
+            <View className="pt-4">
+              <Text className="text-3xl font-bold py-2">{title}</Text>
+
+              <View className="flex-row space-x-2 my-1">
+                <View className="flex-row items-center space-x-1">
+                  <StarIcon color="green" opacity={0.5} size={22} />
+                  <Text className="text-xs text-gray-500">
+                    <Text className="text-green-500">{rating}</Text> • {genre}
+                  </Text>
+                </View>
+
+                <View className="flex-row items-center space-x-1">
+                  <MapPinIcon color="gray" opacity={0.4} size={22} />
+                  <Text className="text-xs text-gray-500">
+                    Nearby • {address}
+                  </Text>
+                </View>
+              </View>
+
+              <Text className="text-gray-500 mt-2 pb-4">
+                {shortDescription}
+              </Text>
+            </View>
+          </View>
+
+          <TouchableOpacity className="flex-row items-center space-x-2 px-5 py-3 border-y border-gray-300">
+            <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+
+            <Text className="pl-1 flex-1 text-md font-bold text-gray-500">
+              Have a food allergy?
+            </Text>
+
+            <ChevronRightIcon color={"#00CCBB"} />
+          </TouchableOpacity>
         </View>
 
-        <View style={globalStyles.container}>
-          <View className="pt-4">
-            <Text className="text-3xl font-bold py-2">{title}</Text>
+        <View className={items.length > 0 ? "pb-36" : "pb-0"}>
+          <View style={globalStyles.container}>
+            <Text className="my-4 font-bold text-xl">Menu</Text>
+          </View>
 
-            <View className="flex-row space-x-2 my-1">
-              <View className="flex-row items-center space-x-1">
-                <StarIcon color="green" opacity={0.5} size={22} />
-                <Text className="text-xs text-gray-500">
-                  <Text className="text-green-500">{rating}</Text> • {genre}
-                </Text>
-              </View>
-
-              <View className="flex-row items-center space-x-1">
-                <MapPinIcon color="gray" opacity={0.4} size={22} />
-                <Text className="text-xs text-gray-500">
-                  Nearby • {address}
-                </Text>
-              </View>
-            </View>
-
-            <Text className="text-gray-500 mt-2 pb-4">{shortDescription}</Text>
+          <View className="bg-white">
+            {/* Dishes */}
+            {dishes.map((dish) => (
+              <DishRow
+                key={dish._id}
+                id={dish._id}
+                title={dish.name}
+                shortDescription={dish.short_description}
+                price={dish.price}
+                imgUrl={dish.image}
+              />
+            ))}
           </View>
         </View>
-
-        <TouchableOpacity className="flex-row items-center space-x-2 px-5 py-3 border-y border-gray-300">
-          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
-
-          <Text className="pl-1 flex-1 text-md font-bold text-gray-500">
-            Have a food allergy?
-          </Text>
-
-          <ChevronRightIcon color={"#00CCBB"} />
-        </TouchableOpacity>
-      </View>
-
-      <View>
-        <View style={globalStyles.container}>
-          <Text className="my-4 font-bold text-xl">Menu</Text>
-        </View>
-
-        <View className="bg-white">
-          {/* Dishes */}
-          {dishes.map((dish) => (
-            <DishRow
-              key={dish._id}
-              id={dish._id}
-              title={dish.name}
-              shortDescription={dish.short_description}
-              price={dish.price}
-              imgUrl={dish.image}
-            />
-          ))}
-        </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </>
   );
 };
 
