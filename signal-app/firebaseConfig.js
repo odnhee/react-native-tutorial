f556624e8b1516dd07ab52f12213681f84667635
HyungJinHan <han1210_36@naver.com>
2023-06-08 17:09:04 +0900

    에러 일시 해결

--- signal-app/firebaseConfig.js
+++ signal-app/firebaseConfig.js
@@ -1,4 +1,4 @@
-import * as firebase from "firebase";
+import { initializeApp } from "firebase/app";
 import {
   API_KEY,
   AUTH_DOMAIN,
@@ -19,15 +19,4 @@ const firebaseConfig = {
   measurementId: MEASUREMENT_ID,
 };
 
-let app;
-
-if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig);
-} else {
-  app = firebase.app();
-}
-
-const db = app.firestore();
-const auth = firebase.auth();
-
-export { db, auth };
+const app = initializeApp(firebaseConfig);
