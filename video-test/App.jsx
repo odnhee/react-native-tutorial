0f1e722bd629746319c87e21b9dafd450303948f
HyungJinHan <han1210_36@naver.com>
2023-06-20 15:12:09 +0900

    코드 정리

--- video-test/App.jsx
+++ video-test/App.jsx
@@ -196,13 +196,6 @@ export default function App() {
     schedulePushNotification(title, body, type);
   };
 
-  const loginWithKakao = async () => {
-    const token = await KakaoLogins.login();
-    // 로그인하기
-    const profile = await KakaoLogins.getProfile();
-    // 프로필 가져오기
-  };
-
   return (
     <View
       style={[
