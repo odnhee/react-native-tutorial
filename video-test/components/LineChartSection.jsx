308a450eabb53619a63ee642e449442c43da84e6
HyungJinHan <han1210_36@naver.com>
2023-06-28 14:22:35 +0900

    PH 수치 데이터 불러오기 추가 및 Flashmessage 추가

--- video-test/components/LineChartSection.jsx
+++ video-test/components/LineChartSection.jsx
@@ -1,23 +1,34 @@
 import { View, Text, Dimensions, ScrollView } from "react-native";
 import React from "react";
 import { LineChart } from "react-native-chart-kit";
-import { useBuoyOxygen } from "../hooks/useBuoyOxygen";
+import { useBuoyOxygen, useBuoyPh } from "../hooks/useBuoyOxygen";
 import { styles } from "../config/globalStyles";
+import FlashMessage, { showMessage } from "react-native-flash-message";
 
 const chartConfig = {
   color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-  strokeWidth: 2, // optional, default 3
   barPercentage: 0.5,
-  useShadowColorFromDataset: false, // optional
+  useShadowColorFromDataset: true, // optional
   backgroundColor: "#f0f0f0",
   backgroundGradientFrom: "#f0f0f0",
   backgroundGradientTo: "#f0f0f0",
+  decimalPlaces: 1, // optional, defaults to 2dp
+  propsForDots: {
+    r: "4",
+    // stroke: "#ffa726",
+  },
 };
 
 const screenWidth = Dimensions.get("window").width;
 
 const LineChartSection = ({ id }) => {
   const { status, data, error, isFetching } = useBuoyOxygen(id);
+  const {
+    status: phStatus,
+    data: phsData,
+    error: phError,
+    isFetching: phIsFetching,
+  } = useBuoyPh(id);
 
   const temperatureData = {
     // labels: data?.map((res) => [res?.measured_time]),
@@ -25,38 +36,56 @@ const LineChartSection = ({ id }) => {
     datasets: [
       {
         data: data?.map((res) => [res?.temperature]),
-        color: (opacity = 1) => `rgba(50, 50, 50, ${opacity})`, // optional
-        strokeWidth: 5, // optional
+        color: (opacity = 1) => `rgba(54, 103, 236, ${opacity})`, // optional
+        strokeWidth: 2, // optional
       },
     ],
-    legend: ["수온"], // optional
+    legend: ["수온 (℃)"], // optional
   };
 
-  const perData = {
+  const mplData = {
     // labels: data?.map((res) => [res?.measured_time]),
     labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     datasets: [
       {
-        data: data?.map((res) => [res?.oxygen_per]),
-        color: (opacity = 1) => `rgba(50, 50, 50, ${opacity})`, // optional
-        strokeWidth: 5, // optional
+        data: data?.map((res) => [res?.oxygen_mpl]),
+        color: (opacity = 1) => `rgba(54, 103, 236, ${opacity})`, // optional
+        strokeWidth: 2, // optional
       },
     ],
-    legend: ["용존산소"], // optional
+    legend: ["용존 산소 (mg/l)"], // optional
   };
 
-  const ppmData = {
+  const phData = {
     // labels: data?.map((res) => [res?.measured_time]),
     labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     datasets: [
       {
-        data: data?.map((res) => [res?.oxygen_ppm]),
-        color: (opacity = 1) => `rgba(50, 50, 50, ${opacity})`, // optional
-        strokeWidth: 5, // optional
+        data: phsData?.map((res) => [res?.ph]),
+        color: (opacity = 1) => `rgba(54, 103, 236, ${opacity})`, // optional
+        strokeWidth: 2, // optional
       },
     ],
-    legend: ["수소 이온 농도"], // optional
+    legend: ["수소 이온 농도 (ph)"], // optional
   };
+
+  if (phIsFetching) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  /** 아래 코드로 에러 핸들링 끝 */
+  if (phStatus === "error") {
+    // status -> success, loading, error...
+    return (
+      <View style={styles.container}>
+        <Text>Error : {phError.message}</Text>
+      </View>
+    );
+  }
 
   if (isFetching) {
     return (
@@ -77,68 +106,94 @@ const LineChartSection = ({ id }) => {
   }
 
   return (
-    <ScrollView>
-      {data[0] === undefined || data.length < 10 ? (
-        <View style={{ alignItems: "center" }}>
-          <Text>수집된 데이터가 존재하지 않습니다.</Text>
-        </View>
-      ) : (
-        <View style={{ gap: 20, alignItems: "center", paddingVertical: 20 }}>
-          <LineChart
-            data={temperatureData}
-            width={screenWidth - 30}
-            height={220}
-            chartConfig={chartConfig}
-            style={{
-              borderRadius: 15,
-              shadowOffset: {
-                width: 10,
-                height: 10,
-              },
-              shadowOpacity: 0.5,
-              shadowRadius: 15,
-              elevation: 5,
-            }}
-            bezier
-          />
-          <LineChart
-            data={perData}
-            width={screenWidth - 30}
-            height={220}
-            chartConfig={chartConfig}
-            style={{
-              borderRadius: 15,
-              shadowOffset: {
-                width: 10,
-                height: 10,
-              },
-              shadowOpacity: 0.5,
-              shadowRadius: 15,
-              elevation: 5,
-            }}
-            bezier
-          />
+    <>
+      <FlashMessage duration={2000} position="bottom" />
 
-          <LineChart
-            data={ppmData}
-            width={screenWidth - 30}
-            height={220}
-            chartConfig={chartConfig}
-            style={{
-              borderRadius: 15,
-              shadowOffset: {
-                width: 10,
-                height: 10,
-              },
-              shadowOpacity: 0.5,
-              shadowRadius: 15,
-              elevation: 5,
-            }}
-            bezier
-          />
-        </View>
-      )}
-    </ScrollView>
+      <ScrollView>
+        {data[0] === undefined || data.length < 10 ? (
+          <View style={{ alignItems: "center" }}>
+            <Text>수집된 데이터가 존재하지 않습니다.</Text>
+          </View>
+        ) : (
+          <View style={{ gap: 20, alignItems: "center", paddingVertical: 20 }}>
+            <LineChart
+              onDataPointClick={({ value, getColor }) =>
+                showMessage({
+                  message: "수온",
+                  description: `${parseFloat(value).toFixed(2)} ℃`,
+                  backgroundColor: getColor(0.7),
+                })
+              }
+              data={temperatureData}
+              width={screenWidth - 30}
+              height={220}
+              chartConfig={chartConfig}
+              style={{
+                borderRadius: 15,
+                shadowOffset: {
+                  width: 10,
+                  height: 10,
+                },
+                shadowOpacity: 0.5,
+                shadowRadius: 15,
+                elevation: 5,
+              }}
+              bezier
+            />
+
+            <LineChart
+              onDataPointClick={({ value, getColor }) =>
+                showMessage({
+                  message: "용존 산소",
+                  description: `${parseFloat(value).toFixed(2)} mg/l`,
+                  backgroundColor: getColor(0.7),
+                })
+              }
+              data={mplData}
+              width={screenWidth - 30}
+              height={220}
+              chartConfig={chartConfig}
+              style={{
+                borderRadius: 15,
+                shadowOffset: {
+                  width: 10,
+                  height: 10,
+                },
+                shadowOpacity: 0.5,
+                shadowRadius: 15,
+                elevation: 5,
+              }}
+              bezier
+            />
+
+            <LineChart
+              onDataPointClick={({ value, getColor }) =>
+                showMessage({
+                  message: "수소 이온 농도",
+                  description: `${parseFloat(value).toFixed(2)} ph`,
+                  backgroundColor: getColor(0.7),
+                })
+              }
+              data={phData}
+              width={screenWidth - 30}
+              height={220}
+              chartConfig={chartConfig}
+              style={{
+                borderRadius: 15,
+                shadowOffset: {
+                  width: 10,
+                  height: 10,
+                },
+                shadowOpacity: 0.5,
+                shadowRadius: 15,
+                elevation: 5,
+              }}
+              bezier
+            />
+          </View>
+        )}
+      </ScrollView>
+    </>
   );
 };
 
--- video-test/hooks/useBuoyOxygen.js
+++ video-test/hooks/useBuoyOxygen.js
@@ -7,6 +7,12 @@ const getOxygenData = async ({ queryKey }) => {
   return await buoyRequest({ url: `/${id}/oxygens/?size=10` });
 };
 
+const getPhData = async ({ queryKey }) => {
+  const id = queryKey[1];
+  // const pageNum = queryKey[2];
+  return await buoyRequest({ url: `/${id}/phs/?size=10` });
+};
+
 export const useBuoyOxygen = (id) => {
   return useQuery(["oxygen", id], getOxygenData, {
     cacheTime: 5 * 60 * 1000, // 5분
@@ -16,8 +22,21 @@ export const useBuoyOxygen = (id) => {
     retry: 2, // error시 fetch 재시도
     select: (data) => {
       const oxygenData = data?.data.results?.map((res) => res);
-      console.log(oxygenData);
       return oxygenData;
+    },
+  });
+};
+
+export const useBuoyPh = (id) => {
+  return useQuery(["ph", id], getPhData, {
+    cacheTime: 5 * 60 * 1000, // 5분
+    staleTime: 1 * 60 * 1000, // 1분
+    refetchOnWindowFocus: true, // 다른 창을 갔다가 돌아왔을 시, refetch
+    refetchOnMount: true,
+    retry: 2, // error시 fetch 재시도
+    select: (data) => {
+      const phData = data?.data.results?.map((res) => res);
+      return phData;
     },
   });
 };
