a854745b8f972d56a52f00ab1f151b4f4ff4ffdd
odnhee <mongheeodn@gmail.com>
2023-07-04 09:45:27 +0900

    정리

--- video-test/App.jsx
+++ video-test/App.jsx
@@ -10,74 +10,54 @@ import KakaoLogin from "./screens/KakaoLogin";
 import BuoyInfo from "./screens/BuoyInfo";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import BuoyDetail from "./screens/BuoyDetail";
-import { backAction, backgroundAction } from "./utils/appStateManager";
-import { AppState } from "react-native";
+import { backAction, backgroundAction } from "./utils/backActions";
+import { RecoilRoot } from "recoil";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const appState = useRef(AppState.currentState);
-  const [appStateVisible, setAppStateVisible] = useState(appState.current);
-  const [is, setIs] = useState(true);
-
+  const [hi, setHi] = useState(true);
   useEffect(() => {
-    const subscription = AppState.addEventListener("change", (nextAppState) => {
-      if (
-        appState.current.match(/inactive|background/) &&
-        nextAppState === "active"
-      ) {
-        setIs(true);
-        console.log("App has come to the foreground!");
-      } else if (
-        appState.current.match(/active/) &&
-        nextAppState === "background"
-      ) {
-        setIs(false);
-        console.log("background!");
-      }
+    // if (url === "Home") {
+    //   const backHandler = BackHandler.addEventListener(
+    //     "hardwareBackPress",
+    //     backAction
+    //     // () =>backAction(setHi)
+    //   );
 
-      appState.current = nextAppState;
-      setAppStateVisible(appState.current);
-      console.log("AppState", appState.current);
-    });
+    //   return () => backHandler.remove();
+    // }
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      // backAction
+      () => backAction(setHi)
+    );
 
-    return () => {
-      subscription.remove();
-    };
-  }, []);
-  useEffect(() => {
-    console.log("appStateVisible", appStateVisible);
+    return () => backHandler.remove();
   }, []);
   /**
    * 앱 완전 종료 이벤트
    * "appstate",를 통해서
    */
-  // useEffect(() => {
-  //   const backHandler = BackHandler.addEventListener(
-  //     "hardwareBackPress",
-  //     () => {
-  //       console.log("hihih");
-  //     }
-  //     // backAction
-  //   );
-
-  //   return () => backHandler.remove();
-  // }, []);
 
   const queryClient = new QueryClient();
-
+  useEffect(() => {
+    setHi(true);
+  }, [hi]);
   return (
     <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <StatusBar style={"auto"} />
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          {/* <Stack.Screen name="/" component={Login} />
-          <Stack.Screen name="KakaoLogin" component={KakaoLogin} /> */}
-          <Stack.Screen name="Home" component={Main} />
-          <Stack.Screen name="Buoy" component={BuoyInfo} />
-          <Stack.Screen name="BuoyDetail" component={BuoyDetail} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <RecoilRoot>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            {/* <Stack.Screen name="/" component={Login} />
+            <Stack.Screen name="KakaoLogin" component={KakaoLogin} /> */}
+            <Stack.Screen name="Home" component={Main} />
+            <Stack.Screen name="Buoy" component={BuoyInfo} />
+            <Stack.Screen name="BuoyDetail" component={BuoyDetail} />
+          </Stack.Navigator>
+          <StatusBar style={"auto"} />
+        </NavigationContainer>
+      </RecoilRoot>
     </QueryClientProvider>
   );
 }
--- video-test/utils/backActions.js
+++ video-test/utils/backActions.js
@@ -1,10 +1,16 @@
 import { Alert, BackHandler } from "react-native";
 
-export const backAction = () => {
+export const backAction = (setHi) => {
   Alert.alert("", "앱을 종료하시겠습니까?", [
     { text: "취소", onPress: () => null },
     // { text: "확인", onPress: () => RNExitApp.exitApp() },
-    { text: "확인", onPress: () => BackHandler.exitApp() },
+    {
+      text: "확인",
+      onPress: () => {
+        setHi(false);
+        BackHandler.exitApp();
+      },
+    },
   ]);
 
   return true;
